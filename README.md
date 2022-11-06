37599218dbc8f0bc0c4e4eac44388e90f8d50b36
KayJuno <dangdan2807@gmail.com>
2022-11-06 10:40:52 +0700

    new version

--- README.md
+++ README.md
@@ -1,21 +1,21 @@
-﻿# kien-truc-va-thiet-ke-pm
+﻿# Lab9
 
-## LAB 1  
-**ActiveMQDemo**: ActiveMQ gửi dữ liệu  
-**ActiveMQDemoReceiver**: ActiveMQDemo nhận dữ liệu
+### get all  
+![getall](../image/lab9/getall_request.png)  
+![redis](../image/lab9/getall_redis.png)  
 
-## LAB 2  
-**PubSub_Publisher** demo trên console.  
-**PubSub_Subcriber** demo trên console.  
-**PubSub_GUI_1** demo trên Gui 1.  
-**PubSub_GUI_2** demo trên Gui 2.  
+### add - update  
+![add](../image/lab9/add.png)  
+![add_redis](../image/lab9/add_redis.png) 
 
-## LAB 3  
-**SpringBoot_JWT**: Project của thầy  
-**SpringBoot_JWT_2**: Project khác  
+#### update
+![update](../image/lab9/update.png)  
+kết quả get by id
 
-## LAB 4 - 5  
-**QuanLyChuyenBay**
+### get by id  
+![getbyid](../image/lab9/get_by_id_redis.png)  
+![redis](../image/lab9/getall_redis.png)  
 
-## LAB - 6
-**spring-boot-active-mq**
+### delete  
+![delete](../image/lab9/delete.png)  
+![redis](../image/lab9/delete_redis.png)  
--- lab9/spring-data-redis/src/main/java/com/javatechie/redis/respository/ProductRepository.java
+++ lab9/spring-data-redis/src/main/java/com/javatechie/redis/respository/ProductRepository.java
@@ -1,36 +1,56 @@
 package com.javatechie.redis.respository;
 
 
+import com.google.gson.Gson;
 import com.javatechie.redis.entity.Product;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.stereotype.Repository;
+import org.apache.logging.log4j.Logger;
+import org.apache.logging.log4j.LogManager;
 
-import java.util.List;
+import java.util.*;
 
 @Repository
 public class ProductRepository {
+    private static Logger logger = LogManager.getLogger(ProductRepository.class);
 
     public static final String HASH_KEY = "Product";
+    static Gson gson = new Gson();
+
     @Autowired
     private RedisTemplate template;
 
-    public Product save(Product product){
-        template.opsForHash().put(HASH_KEY,product.getId(),product);
+    public Product save(Product product) {
+        String data = gson.toJson(product);
+        logger.info(data);
+        template.opsForValue().set(HASH_KEY + "_" + product.getId(), data);
         return product;
     }
 
-    public List<Product> findAll(){
-        return template.opsForHash().values(HASH_KEY);
+    public List<Product> findAll() {
+        Collection<String> collection = new ArrayList<>();
+        collection.add("Product_1");
+        collection.add("Product_2");
+        collection.add("Product_3");
+        List<String> result = template.opsForValue().multiGet(collection);
+        List<Product> productsList = new ArrayList<>();
+        result.forEach(item -> {
+            Product product = gson.fromJson(item, Product.class);
+            productsList.add(product);
+        });
+        return productsList;
     }
 
-    public Product findProductById(int id){
-        return (Product) template.opsForHash().get(HASH_KEY,id);
+    public Product findProductById(int id) {
+        String data = (String) template.opsForValue().get(HASH_KEY + "_" +  id);
+        Product product = gson.fromJson(data, Product.class);
+        return product;
     }
 
 
-    public String deleteProduct(int id){
-         template.opsForHash().delete(HASH_KEY,id);
+    public String deleteProduct(int id) {
+        template.delete(HASH_KEY + "_" +  id);
         return "product removed !!";
     }
 }
